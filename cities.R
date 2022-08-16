53ac9239e262346591574c1ba6aa86a22785978e
vamsy19 <r.vamsy19@gmail.com>
2022-08-17 04:42:27 +0530

    Added a comment line. Wanted to test git difftool

---
+++ cities.R
@@ -0,0 +1,12 @@
+Mumbai<-weather("VABB","2015-01-01","201-06-11")
+
+Bangalore<-weather("VOBG","2015-01-01","2018-06-11")
+
+Hyderabad<-weather("VOHY","2015-01-01","2018-06-11")
+
+Delhi<-weather("VIDD","2015-01-01","2018-06-11")
+Delhi2<-weather("VIDP","2015-01-01","2018-06-11")
+
+Chennai<-weather("VOMM","2015-01-01","2018-06-11")
+
+# xyz
